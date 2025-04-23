b465831aa4787315de7e40da52246548f68bae9a
JustinKSU <JustinKSU@gmail.com>
2025-04-23 04:15:10 -0500

    Jutoma Logo

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
-  return (
-    <nav>
-      <NavLink to="/jutoma.com">Home</NavLink>
-    </nav>
-  );
+    return (
+        <nav>
+            <NavLink to="/jutoma.com"><img src='/favicon.ico' alt='logo'/></NavLink>
+        </nav>
+    );
 };
 
 export default Header;
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 const HomePage = () => (
-  <div className="p-5 mb-4 bg-body-tertiary rounded-3">
-    <h1>Jutoma LLC</h1>
-  </div>
+    <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+        <h1>Jutoma LLC</h1>
+        <ol>
+            <li><a href="https://JustinKSU.com">Justin Rosenberg&apos;s Profile</a></li>
+            <li><a
+                href="https://docs.google.com/document/d/1cyVSwXj8iqqpnR9oanGmfAKlSwAwjccpQ1pYV65-5pM/edit?usp=sharing">Justin
+                Rosenberg&apos;s Resume</a></li>
+            <li>Call me: (913) 543-1498</li>
+        </ol>
+    </div>
 );
 
 export default HomePage;
