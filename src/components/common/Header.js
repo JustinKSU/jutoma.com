83000e374a26ca9b55e5f9af412374d38595effa
JustinKSU <JustinKSU@gmail.com>
2025-04-26 07:23:57 -0500

    Logo

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import logo from "./logo.png";
 
 const Header = () => {
     return (
         <nav>
-            <NavLink to="/jutoma.com"><img src='/favicon.ico' alt='logo'/></NavLink>
+            <NavLink to="/jutoma.com"><img src={logo} alt='logo'/></NavLink>
         </nav>
     );
 };
