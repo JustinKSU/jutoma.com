5157558026b8493c969ce95c4585a5bc3d7ebfc4
JustinKSU <JustinKSU@gmail.com>
2025-11-29 10:52:39 -0600

    Upgraded versions

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    hot: true,
+    port: 3000,
+    historyApiFallback: true
+  }
+};
